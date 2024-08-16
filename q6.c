d29b6b22c3ed1473ccc5a758a16540da81f00757
MBUYt0n <shusrith03@gmail.com>
2024-08-16 15:25:59 +0530

    hmm

--- q6.c
+++ q6.c
@@ -10,17 +10,27 @@ char *convert(char *s, int numRows) {
     int n = 1;
     for (int i = 0; i < strlen(s); i++) {
         t[i] = n;
-        if (n == numRows || n == 1) {
-            sign *= -1
-        }
         n += sign;
+        if (n == numRows || n == 1) {
+            sign *= -1;
+        }
     }
 
-    for (int i = 1; i <= strlen(s); i++) {
+    for (int i = 1; i <= numRows; i++) {
+        for (int j = 0; j < strlen(s); j++) {
+            if (t[j] == i) {
+                x[idx] = s[j];
+                idx++;
+            }
+        }
     }
+
+    return x;
 }
 
 int main() {
-    char s[] = "PAYPALISHIRING";
-    char *x = (char *)malloc(sizeof(char) * strlen(s))
+    char s[] = "AB";
+    char *x = (char *)malloc(sizeof(char) * strlen(s));
+    x = convert(s, 1);
+    printf("%s\n", x);
 }
